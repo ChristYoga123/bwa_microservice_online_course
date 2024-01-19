5e7718894a1fc498edf6fc04e4cabd49d053f4bb
ChristYoga123 <christianuswibisono@gmail.com>
2024-01-19 15:19:32 +0700

    [FEAT] finish get media and delete media

--- be/media-service/app/controllers/mediaController.js
+++ be/media-service/app/controllers/mediaController.js
@@ -3,6 +3,7 @@ const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 const { error, success } = require('../helpers/responseHelper');
 const prisma = new PrismaClient();
+const fs = require('fs');
 
 async function store(req, res) {
     const {image} = req.body;
@@ -25,6 +26,39 @@ async function store(req, res) {
     })
 }
 
+async function index(req, res) {
+    const images = await prisma.media.findMany();
+    return res.status(200).json(success(200, 'Image list', images));
+}
+
+async function destroy(req, res) {
+    const {id} = req.params;
+    const image = await prisma.media.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    })
+    if(!image) {
+        return res.status(404).json(error(404, 'Image not found'));
+    }
+
+    const imagePath = image.image.replace(`${req.get('host')}/`, '');
+    fs.unlink(`./public/${imagePath}`, async (err) => {
+        if(err) {
+            return res.status(400).json(error(400, err.message));
+        }
+
+        await prisma.media.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        return res.status(200).json(success(200, 'Image deleted'));
+    })
+}
+
 module.exports = {
-    store
+    store,
+    index,
+    destroy
 }
--- be/media-service/routes/media.js
+++ be/media-service/routes/media.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const mediaController = require('../app/controllers/mediaController');
 
 router.post("/", mediaController.store)
+router.get("/", mediaController.index)
+router.delete("/:id", mediaController.destroy)
 
 module.exports = router;
